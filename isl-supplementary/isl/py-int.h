54017b669f78ae0bff64dd6dcb5bd4864c66659f
Andreas Kloeckner <inform@tiker.net>
2013-06-19 01:01:18 -0400

    Yet more uninspiring work on Python implementation of isl_int

--- isl-supplementary/isl/py-int.h
+++ isl-supplementary/isl/py-int.h
@@ -36,6 +36,8 @@ typedef PyObject **isl_int;
 
 // }}}
 
+// {{{ init/set/get
+
 inline void isl_int_init(isl_int i)
 {
   isl_int result = malloc(sizeof(PyObject *));
@@ -94,6 +96,8 @@ inline isl_int_get_d(isl_int r)
   return PyLong_AsDouble(*r)
 }
 
+// }}}
+
 inline char *isl_int_get_str(isl_int r)
 {
   PyObject *str = PyObject_Str(*r);
@@ -109,14 +113,16 @@ inline void isl_int_abs(isl_int r, isl_int i)
 {
   Py_DECREF(*r);
   *r = PyNumber_Absolute(*i);
-  if (!*r) ISL_INT_PY_HANDLE_PY_ERROR(isl_int_abs);
+  if (!*r)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_abs);
 }
 
 inline void isl_int_neg(isl_int r, isl_int i)
 {
   Py_DECREF(*r);
   *r = PyNumber_Negative(*i);
-  if (!*r) ISL_INT_PY_HANDLE_PY_ERROR(isl_int_abs);
+  if (!*r)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_abs);
 }
 
 inline void isl_int_swap(isl_int i, isl_int j)
@@ -144,21 +150,25 @@ ISL_INT_PY_THREE_OP_UI_FUNC(mul_2exp, PyNumber_LShift);
 ISL_INT_PY_THREE_OP_UI_FUNC(mul_ui, PyNumber_Multiply);
 ISL_INT_PY_THREE_OP_UI_FUNC(pow_ui, PyNumber_Power);
 
+// }}}
+
+// FIXME
 isl_int_addmul(isl_int r, isl_int i, isl_int j)
+// FIXME
 isl_int_submul(isl_int r, isl_int i, isl_int j)
 
+// FIXME
 isl_int_gcd(isl_int r, isl_int i, isl_int j)
+// FIXME
 isl_int_lcm(isl_int r, isl_int i, isl_int j)
 
 // FIXME: not exploiting performance increase for exactness
 ISL_INT_PY_THREE_OP_FUNC(divexact, PyNumber_FloorDiv);
 ISL_INT_PY_THREE_OP_UI_FUNC(divexact_ui, PyNumber_FloorDiv);
 
-// }}}
-
 // FIXME
 isl_int_tdiv_q(isl_int r, isl_int i, isl_int j)
-  //
+
 // FIXME
 isl_int_cdiv_q(isl_int r, isl_int i, isl_int j)
 
@@ -169,30 +179,73 @@ isl_int_fdiv_r(isl_int r, isl_int i, isl_int j)
 
 ISL_INT_PY_THREE_OP_UI_FUNC(fdiv_q_ui, PyNumber_FloorDiv);
 
+// {{{ I/O / string conversion
+
 inline void isl_int_read(isl_int r, const char *s)
 {
   Py_DECREF(*r);
   *r = PyLong_FromString(s, NULL, 10);
-  if (!*r) ISL_INT_PY_HANDLE_PY_ERROR(isl_int_read);
+  if (!*r)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_read);
 }
 
-// FIXME
-#define isl_int_print(out, i,width)                                     \
-        do {                                                            \
-                char *s;                                                \
-                isl_int_print_gmp_free_t gmp_free;                      \
-                s = mpz_get_str(0, 10, i);                              \
-                fprintf(out, "%*s", width, s);                          \
-                mp_get_memory_functions(NULL, NULL, &gmp_free);         \
-                (*gmp_free)(s, strlen(s)+1);                            \
-        } while (0)
+inline void isl_int_print(FILE *out, isl_int i, int width)
+{
+  PyObject *py_str = PyObject_Str(*i);
+  if (!py_str)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_print);
+  char *s = PyString_AsString(PyObject *string)
+  if (!s)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_print);
+  fprintf(out, "%*s", width, s);
+  Py_DECREF(py_str);
+}
+
+// }}}
+
+// {{{ cmp, sgn
 
 #define isl_int_sgn(i) isl_int_cmp_si(i, 0)
 
-// FIXME
-isl_int_cmp(isl_int i, isl_int j)
-// FIXME
-isl_int_cmp_si(isl_int i,si)
+inline int isl_int_cmp(isl_int i, isl_int j)
+{
+  int res = PyObject_RichCompareBool(*i, *j, Py_GT); \
+  if (res == -1)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_cmp);
+  if (res == 1)
+    return 1;
+  int res = PyObject_RichCompareBool(*i, *j, Py_EQ); \
+  if (res == -1)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_cmp);
+  if (res == 1)
+    return 0;
+  return -1;
+}
+
+inline int isl_int_cmp_si(isl_int i, long int si)
+{
+  PyObject *j = PyLong_FromLong(si);
+  if (!j)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_cmp_si);
+
+  int res = PyObject_RichCompareBool(*i, j, Py_GT); \
+  if (res == -1)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_cmp_si);
+  if (res == 1)
+  {
+    Py_DECREF(j);
+    return 1;
+  }
+  int res = PyObject_RichCompareBool(*i, j, Py_EQ); \
+  if (res == -1)
+    ISL_INT_PY_HANDLE_PY_ERROR(isl_int_cmp_si);
+  Py_DECREF(j);
+  if (res == 1)
+    return 0;
+  return -1;
+}
+
+// }}}
 
 // {{{ "rich" comparisons
 
