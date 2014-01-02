9af2936ca293797c3a87ad8049e746f8287acf2e
Andreas Kloeckner <inform@tiker.net>
2014-01-03 00:24:49 +0100

    Minor example fixes

--- examples/demo.py
+++ examples/demo.py
@@ -4,8 +4,8 @@ ctx = isl.Context()
 space = isl.Space.create_from_names(ctx, set=["x", "y"])
 
 bset = (isl.BasicSet.universe(space)
-        .add_constraint(isl.Constraint.ineq_from_names(space, {1: -1, "x":1}))
-        .add_constraint(isl.Constraint.ineq_from_names(space, {1: 5, "x":-1}))
+        .add_constraint(isl.Constraint.ineq_from_names(space, {1: -1, "x": 1}))
+        .add_constraint(isl.Constraint.ineq_from_names(space, {1: 5, "x": -1}))
         .add_constraint(isl.Constraint.ineq_from_names(space, {1: -1, "y": 1}))
         .add_constraint(isl.Constraint.ineq_from_names(space, {1: 5, "y": -1})))
 print "set 1:", bset
@@ -15,11 +15,13 @@ print "set 2:", bset2
 
 bsets_in_union = []
 bset.union(bset2).coalesce().foreach_basic_set(bsets_in_union.append)
+print bsets_in_union
 union, = bsets_in_union
 print "union:", union
 # ENDEXAMPLE
 
 import matplotlib.pyplot as pt
+
 
 def plot_basic_set(bset, *args, **kwargs):
     # This is a total hack. But it works for what it needs to do. :)
@@ -48,7 +50,7 @@ def plot_basic_set(bset, *args, **kwargs):
             sorted(vertex_pts, key=lambda (x, y): atan2(y-center[0], x-center[1])))
 
     if plot_vert:
-        pt.plot(vertex_pts[:,0], vertex_pts[:,1], "o")
+        pt.plot(vertex_pts[:, 0], vertex_pts[:, 1], "o")
 
     import matplotlib.path as mpath
     import matplotlib.patches as mpatches
@@ -76,5 +78,6 @@ pt.ylim([-1, 8])
 #pt.show()
 pt.savefig("before-union.png", dpi=50)
 
-plot_basic_set(union, facecolor='blue', edgecolor='yellow', alpha=0.5, plot_vert=True)
+plot_basic_set(union, facecolor='blue', edgecolor='yellow',
+        alpha=0.5, plot_vert=True)
 pt.savefig("after-union.png", dpi=50)
