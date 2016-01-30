5e681433e08e34c5a0dc2dc9aad358d85e1bb96b
Andreas Kloeckner <inform@tiker.net>
2016-01-29 18:19:08 -0600

    Turn SIO off by default (trouble with old linkers), but do run a test build with it on

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def get_config_schema():
         Switch("USE_SHIPPED_ISL", True, "Use included copy of isl"),
         Switch("USE_SHIPPED_IMATH", True, "Use included copy of imath in isl"),
         Switch("USE_BARVINOK", False, "Include wrapper for Barvinok"),
-        Switch("USE_IMATH_SIO", True, "When using imath, use small-integer "
+        Switch("USE_IMATH_SIO", False, "When using imath, use small-integer "
             "optimization"),
 
         IncludeDir("GMP", []),
