e227e43ea41e81d1c8a37aca59bff4aa8df1c8c8
Andreas Kloeckner <inform@tiker.net>
2018-05-29 18:41:13 +0200

    Re-fix after merge of other demo fix

--- examples/demo.py
+++ examples/demo.py
@@ -9,8 +9,7 @@ bset = (isl.BasicSet.universe(space)
         .add_constraint(isl.Constraint.ineq_from_names(space, {1: 5, "y": -1})))
 print("set 1 %s:" % bset)
 
-bset2 = isl.BasicSet("{[x, y] : x >= 0 and x < 5 and y >= 0 and y < x+4 }",
-                     context=ctx)
+bset2 = isl.BasicSet("{[x, y] : x >= 0 and x < 5 and y >= 0 and y < x+4 }")
 print("set 2: %s" % bset2)
 
 bsets_in_union = []
