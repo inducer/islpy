8af69b9dea6d2130001b87b61fc42397adf870f1
Andreas Kloeckner <inform@tiker.net>
2020-11-03 21:42:40 -0600

    Upgrade to, establish compatibility with isl 0.23

--- gen_wrap.py
+++ gen_wrap.py
@@ -283,7 +283,8 @@ FUNC_PTR_RE = re.compile(r"""
     \)
     """,
     re.VERBOSE)
-STRUCT_DECL_RE = re.compile(r"(__isl_export\s+)?struct\s+([a-z_A-Z0-9]+)\s*;")
+STRUCT_DECL_RE = re.compile(
+        r"(__isl_export\s+)?struct\s+(__isl_export\s+)?([a-z_A-Z0-9]+)\s*;")
 ARG_RE = re.compile(r"^((?:\w+)\s+)+(\**)\s*(\w+)$")
 INLINE_SEMICOLON_RE = re.compile(r"\;[ \t]*(?=\w)")
 SUBCLASS_RE = re.compile(
