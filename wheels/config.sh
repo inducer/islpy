c275854835867633e7641fd870ece318516fb637
Andreas Kloeckner <inform@tiker.net>
2023-01-01 14:46:52 -0600

    Drop gmp from wheel build

--- wheels/config.sh
+++ wheels/config.sh
@@ -38,15 +38,8 @@ function pre_build {
         export ABI=32
     fi
     echo "Bundled dependencies in the wheel" >> doc/misc.rst
-    build_simple2 gmp  6.1.2 https://gmplib.org/download/gmp tar.bz2 \
-        --enable-shared --disable-static --with-pic --enable-fat
-    pushd gmp-6.1.2
-      cat README >> ../doc/misc.rst
-      cat COPYING.LESSERv3 >> ../doc/misc.rst
-      echo "" >> ../doc/misc.rst
-    popd
     build_simple2 isl 0.24 https://libisl.sourceforge.io tar.gz \
-        --enable-shared --disable-static --with-int=gmp --with-gmp-prefix=$BUILD_PREFIX
+        --enable-shared --disable-static --with-int=imath-32
     pushd isl-0.24
       cat LICENSE >> ../doc/misc.rst
     popd
@@ -61,7 +54,7 @@ function run_tests {
 function pip_wheel_cmd {
     local abs_wheelhouse=$1
     rm -rf siteconf.py
-    python configure.py --no-use-shipped-isl --isl-inc-dir=$BUILD_PREFIX/include --isl-lib-dir=$BUILD_PREFIX/lib
+    python configure.py --no-use-gmp --no-use-shipped-isl --isl-inc-dir=$BUILD_PREFIX/include --isl-lib-dir=$BUILD_PREFIX/lib
     pip wheel $(pip_opts) -w $abs_wheelhouse --no-deps .
 }
 
