c9750f99e0ca0b5482816f6ddf88518ad9dc79c9
Andreas Kloeckner <inform@tiker.net>
2025-05-29 13:00:33 -0500

    Switch to scikit build core

--- islpy/version.py
+++ islpy/version.py
@@ -1,2 +1,11 @@
-VERSION = (2025, 1, 5)
-VERSION_TEXT = ".".join(str(i) for i in VERSION)
+from __future__ import annotations
+
+import re
+from importlib import metadata
+
+
+VERSION_TEXT = metadata.version("islpy")
+_match = re.match(r"^([0-9.]+)([a-z0-9]*?)$", VERSION_TEXT)
+assert _match is not None
+VERSION_STATUS = _match.group(2)
+VERSION = tuple(int(nr) for nr in _match.group(1).split("."))
